99e6aaf9afe5b860c52bd1754f4f81e53b08df6c
clapts <dervishiclaudio@gmail.com>
2025-02-03 21:25:38 +0100

    quarta aprova push

--- src/main/java/it/unipv/ingsw/progn25/ProgettoN25.java
+++ src/main/java/it/unipv/ingsw/progn25/ProgettoN25.java
@@ -5,9 +5,6 @@ public class ProgettoN25 {
     public static void main(String[] args) {
         //Entry point
         System.out.println("Progetto");
-        System.out.println("Progetto");
-        System.out.println("Progetto");
-        System.out.println("Progetto");
-        System.out.println("Progetto");   
+
     }
 }
