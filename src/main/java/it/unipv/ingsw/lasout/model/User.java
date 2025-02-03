5f08578d78857bdf69d047bd8e52dabec36ef402
clapts <dervishiclaudio@gmail.com>
2025-02-03 23:16:02 +0100

    implementazione

--- src/main/java/it/unipv/ingsw/lasout/model/User.java
+++ src/main/java/it/unipv/ingsw/lasout/model/User.java
@@ -1,10 +1,14 @@
 package it.unipv.ingsw.lasout.model;
 
+import it.unipv.ingsw.lasout.model.grup.Group;
+
 import java.util.List;
 
 public class User {
 
+    private long id;
     private List<Group> groups;
+
 
     public List<Group> getGroups() {
         return groups;
