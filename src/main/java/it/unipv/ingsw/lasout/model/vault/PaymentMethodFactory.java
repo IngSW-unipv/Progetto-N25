70774387d5c96e45aa20b8b8cc9b1bb25a14c340
LorenzoB0405 <lorenzobusin04@gmail.com>
2025-02-06 00:37:44 +0100

    Creazione interfaccia Vault

--- src/main/java/it/unipv/ingsw/lasout/model/vault/PaymentMethodFactory.java
+++ src/main/java/it/unipv/ingsw/lasout/model/vault/PaymentMethodFactory.java
@@ -1,16 +1,15 @@
 package it.unipv.ingsw.lasout.model.vault;
-import it.unipv.ingsw.lasout.model.user.User;
 
 public class PaymentMethodFactory {
 
-	public static PaymentMethod createPayment(String method, User user, String numeroCarta, String dataScadenza, String cvv, String iban) {
+	public static PaymentMethod createPayment(String method, String numeroCarta, String dataScadenza, String cvv, String iban) {
         switch (method.toLowerCase()) {
-            case "credit_card":
-                return new CreditCard(user, numeroCarta, dataScadenza, cvv);
+            case "carta_credito":
+                return new CreditCard(numeroCarta, dataScadenza, cvv);
             case "paypal":
-                return new PayPal(user, numeroCarta);
-            case "bank_transfer":
-                return new CurrentAccount(user, iban);
+                return new PayPal(numeroCarta);
+            case "conto_corrente":
+                return new CurrentAccount(iban);
             default:
                 throw new IllegalArgumentException("Metodo di pagamento '" + method + "' non supportato.");
         }
