ec414c8e4258b8d2e05be3e9f5203921ae71c44a
LorenzoB0405 <lorenzobusin04@gmail.com>
2025-02-17 23:02:59 +0100

    useless file removal

--- vault/paymentmethod/IPaymentMethodDAO.java
+++
@@ -1,9 +0,0 @@
-package it.unipv.ingsw.lasout.model.vault.paymentmethod;
-
-import java.util.List;
-
-import it.unipv.ingsw.lasout.dao.IDao;
-
-public interface IPaymentMethodDAO extends IDao<PaymentMethod>{
-}
-
