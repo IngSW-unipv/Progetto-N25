16ec25fb8c85fb16f77a3cd011d763a54b46cab0
Dada <andreadada.personal@gmail.com>
2025-02-27 22:15:49 +0100

    Init stuff

---
+++ src/main/java/it/unipv/ingsw/lasout/util/InitSaintReim2025.java
@@ -0,0 +1,74 @@
+package it.unipv.ingsw.lasout.util;
+
+public class  InitSaintReim2025{
+
+  private static String testo = "Magari non sarà\n" +
+          "Nemmeno questa sera\n" +
+          "La sera giusta per tornare insieme\n" +
+          "Tornare a stare insieme\n" +
+          "Magari non sarà\n" +
+          "Nemmeno questa sera\n" +
+          "Me l’ha detto la signora, là aﬀacciata al quarto piano\n" +
+          "Con la sigaretta in bocca,\n" +
+          "Mentre stendeva il suo bucato\n" +
+          "Io le ho risposto che\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Tornare a quando\n" +
+          "Ci bastava\n" +
+          "Ridere, piangere, fare l’amore\n" +
+          "E poi stare in silenzio per ore\n" +
+          "Fino ad addormentarci sul divano\n" +
+          "Con il telecomando in mano\n" +
+          "Non so più come fare senza te\n" +
+          "Te che mi fai, vivere e dimenticare,\n" +
+          "Tu che mentre cucini ti metti a cantare\n" +
+          "E tu chiamala se vuoi la fine\n" +
+          "Ma come te lo devo dire\n" +
+          "Sta vita non è vita senza te\n" +
+          "Ma sai che questa sera\n" +
+          "Balorda nostalgia\n" +
+          "Mi accendo la tv\n" +
+          "Solo per farmi compagnia\n" +
+          "Che bella tiritera…\n" +
+          "Beh insomma\n" +
+          "Ti sembra la maniera\n" +
+          "Che vai e mi lasci qua\n" +
+          "Ti cerco ancora in casa quando mi prude la schiena\n" +
+          "E metto ancora un piatto in più quando apparecchio a cena\n" +
+          "So soltanto che vorrei,\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Vorrei\n" +
+          "Sì vorrei\n" +
+          "Vorrei\n" +
+          "Tornare a quando\n" +
+          "Ci bastava\n" +
+          "Ridere, piangere, fare l’amore\n" +
+          "E poi stare in silenzio per ore\n" +
+          "Fino ad addormentarci sul divano\n" +
+          "Con il telecomando in mano\n" +
+          "Non so più come fare senza te\n" +
+          "Te che mi fai, vivere e dimenticare,\n" +
+          "Tu che mentre cucini ti metti a cantare\n" +
+          "E tu chiamala se vuoi la fine\n" +
+          "Ma come te lo devo dire\n" +
+          "Sta vita non è vita senza te\n" +
+          "Ma chissà perché\n" +
+          "Oh, sta vita non è vita senza te\n" +
+          "Magari non sarà\n" +
+          "Magari è già finita\n" +
+          "Però ti voglio bene\n" +
+          "Ed è stata tutta vita";
+
+  static{
+
+    System.out.println(testo);
+
+  }
+
+}
